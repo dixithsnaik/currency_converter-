9ce5e5b026982feb1953b7a5d6772ef17b9913fd
dixithsnaik <dixithsnaik@gmail.com>
2023-10-19 01:47:44 +0530

    border

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,51 +1,44 @@
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
-  
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    const border = OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(5)));
+    return  const Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
-        child : Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          Text( '0', 
-                style: TextStyle(
-                fontSize: 45, 
-                fontWeight: FontWeight.bold, 
-                color : Color.fromARGB(255,255,255,255)
-          ),
-          ),
-          TextField(
-            style: TextStyle(
-              color: Colors.black
-          ),
-          decoration: InputDecoration(
-            
-            hintText: 'Please enter the ammount in USD',
-            hintStyle: TextStyle(
-              color:Colors.black54
+            Text(
+              '0',
+              style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
             ),
-            prefixIcon: Icon(Icons.monetization_on_outlined) ,
-            prefixIconColor: Colors.black, 
-            filled: true,
-            fillColor: Colors.white,
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                
-               color: Colors.white,
-               width: 5.0 
-              )
-            )
-
-          ),
-          ),
-        ],
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                hintText: 'Please enter the ammount in USD',
+                hintStyle: TextStyle(color: Colors.black54),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Color.fromARGB(255, 0, 0, 0),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+            ),
+          ],
+        ),
       ),
-      ), 
     );
-    
   }
 }
